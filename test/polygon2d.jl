ff9552cf23350f8c52de2b26ee71ad2335385a37
tejus-gupta <tejusgupta14@gmail.com>
2017-03-08 23:37:19 +0530

    added test for color

--- test/polygon2d.jl
+++ test/polygon2d.jl
@@ -22,4 +22,12 @@ using Base.Test
     @test all(x->x==true, img[3,1:3])==true
     @test all(x->x==false, img[2,2:3])==true
     @test img[2,4]==true
+
+    img=zeros(ColorTypes.RGB{N0f8},5,5)
+    polygon_perimeter!(img, vert, RGB{N0f8}(1,0,0))
+    @test all(x->x==RGB{N0f8}(1,0,0), img[1,:])==true
+    @test all(x->x==RGB{N0f8}(1,0,0), img[1:3,1])==true
+    @test all(x->x==RGB{N0f8}(1,0,0), img[3,1:3])==true
+    @test all(x->x==RGB{N0f8}(0,0,0), img[2,2:3])==true
+    @test img[2,4]==RGB{N0f8}(1,0,0)
 end
