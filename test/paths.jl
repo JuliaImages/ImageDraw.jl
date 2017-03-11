ef348e4a5fb1ca1e07b4088b51e66f281550e8f1
tejus-gupta <tejusgupta14@gmail.com>
2017-03-11 19:02:19 +0530

    Adds the paths function (#5)
    * added the paths function

---
+++ test/paths.jl
@@ -0,0 +1,41 @@
+using Images
+using Base.Test
+
+@testset "Paths" begin
+    vert=CartesianIndex{2}[]
+    push!(vert, CartesianIndex(1,1))
+    push!(vert, CartesianIndex(1,3))
+    push!(vert, CartesianIndex(1,5))
+    push!(vert, CartesianIndex(3,3))
+    push!(vert, CartesianIndex(3,1))
+
+    img = path(zeros(ColorTypes.Gray{Bool},5,5), vert, closed=true)
+    @test all(x->x==true, img[1,:])==true
+    @test all(x->x==true, img[1:3,1])==true
+    @test all(x->x==true, img[3,1:3])==true
+    @test all(x->x==false, img[2,2:3])==true
+    @test img[2,4]==true
+
+    img=zeros(ColorTypes.Gray{Bool},5,5)
+    path!(img, vert)
+    @test all(x->x==true, img[1,:])==true
+    @test all(x->x==true, img[1:3,1])==true
+    @test all(x->x==true, img[3,1:3])==true
+    @test all(x->x==false, img[2,2:3])==true
+    @test img[2,4]==true
+
+    img=zeros(ColorTypes.RGB{N0f8},5,5)
+    path!(img, vert, RGB{N0f8}(1,0,0), closed=true)
+    @test all(x->x==RGB{N0f8}(1,0,0), img[1,:])==true
+    @test all(x->x==RGB{N0f8}(1,0,0), img[1:3,1])==true
+    @test all(x->x==RGB{N0f8}(1,0,0), img[3,1:3])==true
+    @test all(x->x==RGB{N0f8}(0,0,0), img[2,2:3])==true
+    @test img[2,4]==RGB{N0f8}(1,0,0)
+
+    img=zeros(ColorTypes.RGB{N0f8},5,5)
+    path!(img, vert,closed=false)
+    @test all(x->x==RGB{N0f8}(1,1,1), img[1,:])==true
+    @test all(x->x==RGB{N0f8}(1,1,1), img[3,1:3])==true
+    @test all(x->x==RGB{N0f8}(0,0,0), img[2,1:3])==true
+    @test img[2,4]==RGB{N0f8}(1,1,1)
+end
