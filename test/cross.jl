05c5a15b6abf4a51750bc42b3aea904b1e038988
Yakir Luc Gagnon <12.yakir@gmail.com>
2019-05-17 14:52:57 +0200

    Create cross.jl

---
+++ test/cross.jl
@@ -0,0 +1,12 @@
+@testset "Lines2D" begin
+
+  expected = Gray{N0f8}.([0 0 1 0 0
+                          0 1 1 1 0
+                          0 0 1 0 0
+                          0 0 0 0 0
+                          0 0 0 0 0])
+  img = fill(Gray{N0f8}(0), 5, 5)
+  draw!(img, Cross(Point(3,2), 1), Gray{N0f8}(1))
+  @test img == expected
+  
+end
