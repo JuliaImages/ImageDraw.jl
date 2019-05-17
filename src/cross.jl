1cd9252de59e5d99ed269177c4a0d8270a38f9bd
Yakir Luc Gagnon <12.yakir@gmail.com>
2019-05-17 14:44:53 +0200

    Create cross.jl
    Fixes #24

---
+++ src/cross.jl
@@ -0,0 +1,24 @@
+"""
+    cross = Cross(c, range::UnitRange{Int})
+A `Drawable` cross passing through the point `c` with arms ranging across `range`.
+"""
+struct Cross <: Drawable
+    c::Point
+    range::UnitRange{Int}
+end
+
+"""
+    cross = Cross(c, arm::Int)
+A `Drawable` cross passing through the point `c` with arms that are `arm` pixels long.
+"""
+Cross(c, arm::Int) = Cross(c, -arm:arm)
+
+function draw!(img::AbstractArray{T, 2}, cross::Cross, color::T) where T<:Colorant
+    for Δx in cross.range
+        img[cross.c.y, cross.c.x + Δx] = color
+    end
+    for Δy in cross.range
+        img[cross.c.y + Δy, cross.c.x] = color
+    end
+    img
+end
