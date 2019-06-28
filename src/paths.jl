81bb3e7a341ecb3e1e502852da9f207efffb7897
Codyk12 <cody.kesler@icloud.com>
2019-06-28 12:40:45 -0400

    Added ability to change thickness with default color

--- src/paths.jl
+++ src/paths.jl
@@ -3,6 +3,9 @@
 Path(v::AbstractVector{Tuple{Int, Int}}) = Path([Point(p...) for p in v])
 Path(v::AbstractVector{CartesianIndex{2}}) = Path([Point(p) for p in v])
 
+draw!(img::AbstractArray{T, 2}, path::Path, thickness::Int) where T<:Colorant =
+	draw!(img, path, oneunit(T), thickness)
+
 function draw!(img::AbstractArray{T, 2}, path::Path, color::T, thickness::Int=1) where T<:Colorant
     vertices = [CartesianIndex(p.y, p.x) for p in path.vertices]
     for i in 1:length(vertices)-1
@@ -16,6 +19,9 @@ end
 Polygon(v::AbstractVector{Tuple{Int, Int}}) = Polygon([Point(p...) for p in v])
 Polygon(v::AbstractVector{CartesianIndex{2}}) = Polygon([Point(p) for p in v])
 
+draw!(img::AbstractArray{T, 2}, polygon::Polygon, thickness::Int) where T<:Colorant =
+	draw!(img, path, oneunit(T), thickness)
+
 function draw!(img::AbstractArray{T, 2}, polygon::Polygon, color::T, thickness::Int=1) where T<:Colorant
     draw!(img, Path(polygon.vertices), color, thickness)
     draw!(img, LineSegment(first(polygon.vertices), last(polygon.vertices)), color, thickness)
@@ -25,6 +31,9 @@ end
 
 RegularPolygon(point::CartesianIndex{2}, side_count::Int, side_length::T, θ::U) where {T<:Real, U<:Real} =
     RegularPolygon(Point(point), side_count, side_length, θ)
+
+draw!(img::AbstractArray{T, 2}, rp::RegularPolygon, thickness::Int) where T<:Colorant =
+	draw!(img, rp, oneunit(T), thickness)
 
 function draw!(img::AbstractArray{T, 2}, rp::RegularPolygon, color::T, thickness::Int=1) where T<:Colorant
     n = rp.side_count
